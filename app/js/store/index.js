cbe87cfa6fe08085c3208dc37a6abffd7dea8266
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-06-16 10:08:49 -0400

    chore: Wrap dispatch with logging

--- app/js/store/index.js
+++ app/js/store/index.js
@@ -4,10 +4,29 @@ import throttle from 'lodash/throttle';
 import { loadState, saveState } from './localStorage';
 import todoApp from '../reducers';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) return rawDispatch;
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: blue', action);
+    const res = rawDispatch(action);
+    console.log('%c next state', 'color: green', store.getState());
+    console.groupEnd(action.type);
+    return res;
+  };
+};
+
 // this allows us to create multiple instances, good for testing.
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(todoApp, persistedState);
+
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
 
   store.subscribe(throttle(() => {
     saveState({ todos: store.getState().todos })
