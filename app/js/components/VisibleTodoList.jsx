609413bd3b89b70ebac3b5c63dec0e3cdfd2b885
Zachary R. Smith <ZacharySmith4989@gmail.com>
2016-06-22 11:54:36 -0400

    refactor: Make VisibleTodoList use fake API

--- app/js/components/VisibleTodoList.jsx
+++ app/js/components/VisibleTodoList.jsx
@@ -1,8 +1,30 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import TodoList from './TodoList.jsx';
 import { toggleTodo } from '../actions';
 import { getVisibleTodos } from '../reducers/todoLists';
+import { fetchTodos } from '../api';
+
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    fetchTodos('list1Id', this.props.filter).then(todos => {
+      console.log(this.props.filter, todos);
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.filter === prevProps.filter) return;
+
+    fetchTodos('list1Id', this.props.filter).then(todos => {
+      console.log(this.props.filter, todos);
+    });
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
 const mapStateToProps = (
   state,
@@ -16,4 +38,4 @@ const mapStateToProps = (
 export default connect(
   mapStateToProps,
   { onTodoClick: toggleTodo }
-)(TodoList);
+)(VisibleTodoList);
